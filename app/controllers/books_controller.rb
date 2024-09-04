032187bcfc47f18cf43d02ff1859a3b6960b3c36
root <root@localhost>
2024-09-04 02:30:55 +0000

    Began adding resourceful routes. Finished with homepage and show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,27 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.order(:title)
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end  
+
   def new
+  end
+
+  def create
   end
 
   def edit
   end
 
+  def update
+  end
+
   def delete
   end
 
-  def show
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do
 
   root 'books#index'
 
-  get 'books/index'
-  get 'books/new'
-  get 'books/edit'
-  get 'books/delete'
-  get 'books/show'
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'books/index'
+  # get 'books/new'
+  # get 'books/edit'
+  # get 'books/delete'
+  # get 'books/show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
