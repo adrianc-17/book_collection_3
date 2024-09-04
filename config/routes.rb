5804a5c5e5002dd7738315e3616c489d7a164cd1
root <root@localhost>
2024-09-04 00:24:07 +0000

    generated Books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/delete'
+  get 'books/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
